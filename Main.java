3fd5cab0f9602a96f9fff898caf93fb8b9414c92
kelvincao2002 <43891826+kelvincao2002@users.noreply.github.com>
2018-10-22 11:52:40 -0700

    AR7

--- Main.java
+++ Main.java
@@ -68,7 +68,7 @@ class Main
        /*
        * Task 3. Add 1 to each element in ar1.  Print it out below
        */
-        System.out.println("** Stask 3 **");
+        System.out.println("** Task 3 **");
         int value;
         for (int i=0 ; i<ar1.length ; i++)
         {
@@ -168,14 +168,31 @@ class Main
           *    ar2[2]=2
           *    ar2[3]=3  ->  ar2odds[1]=3
           */
-         
+         int k=0;
+           int[] ar2odds = new int[ar2.length]; // too big right now.
+           for ( int i=0 ; i<ar2.length ; i++)
+           {
+               System.out.println(ar2[i]); // Just print it out to see
+               
+               if (i%2==1)
+               {
+                   ar2odds[k]=ar2[i];
+                   k++;
+               }
+           }
+           
+           for (int i=0 ; i<k ; i++)
+               System.out.println(ar2odds[i]);
+            
         /*
          * Task 9. In the array ar2, count how many odd numbers you
          * have.  Then create an ew array called ar4.  Copy just the odd
          * numbers from ar1 into ar4.  Print ar4
          */
-        
-        /*
+        for (int i=0; i<ar2.length; i++)
+       
+       
+        '''''''''''''''''''*
          * Task 10.  Shift the elements of ar4 right by 1
          * For example
          * old   ar4: 1 3 5 7 9
